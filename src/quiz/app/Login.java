991344efe8e442fc2abe6a43079c53866c00f9fe
Jakub <jdnouzov@gmail.com>
2024-05-02 17:02:58 +0200

    Pridani nadpisu a zadani jmena a tlacitek

--- src/quiz/app/Login.java
+++ src/quiz/app/Login.java
@@ -5,18 +5,52 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static java.awt.Font.TRUETYPE_FONT;
+
 public class Login extends JFrame implements ActionListener {
 
-    JButton rules, back;
+    JButton rules, close;
 
     Login(){
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
 
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login3.jpg"));
         JLabel image = new JLabel(i1);
-        image.setBounds(0,0,1920,1080);
+        image.setBounds(0,0,1000,1080);
         add(image);
+
+        JLabel heading = new JLabel("QUIZ");
+        heading.setBounds(1400,200,200,50);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 50));
+        heading.setForeground(Color.BLACK);
+        add(heading);
+
+        JLabel name = new JLabel("Zadejte jméno");
+        name.setBounds(1375,300,300,90);
+        name.setFont(new Font("Mongolian Baiti", Font.BOLD, 30));
+        name.setForeground(Color.BLACK);
+        add(name);
+
+        JTextField tfname = new JTextField();
+        tfname.setBounds(1388,380,150,25);
+        tfname.setFont(new Font("Times New Roman", TRUETYPE_FONT,20));
+        add(tfname);
+
+        rules = new JButton("Pravidla");
+        rules.setBounds(1403,430,120,25);
+        rules.setFont(new Font("Times New Roman", TRUETYPE_FONT,20));
+        rules.setBackground(Color.BLUE);
+        rules.setForeground(Color.WHITE);
+        add(rules);
+
+        close = new JButton("Zavřít");
+        close.setBounds(1403,460,120,25);
+        close.setFont(new Font("Times New Roman", TRUETYPE_FONT,20));
+        close.setBackground(Color.BLUE);
+        close.setForeground(Color.WHITE);
+        close.addActionListener(this);
+        add(close);
 
         setSize(1920,1080);
         setVisible(true);
