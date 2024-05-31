300d52fce20e887dce7d29ce14e4e337c6dca097
Jakub <jdnouzov@gmail.com>
2024-05-31 23:02:10 +0200

    Přendání metod z Historie do samostatné třídy, abych je mohl používat i v dalších třídách. Vytvoření otázek a odpovědí na další témata

--- src/Quizz/Historie.java
+++ src/Quizz/Historie.java
@@ -2,7 +2,8 @@ package Quizz;
 
 import loadFiles.LoadAnswers;
 import loadFiles.LoadQuestions;
-import quiz.app.ReakceOdpovedi;
+import quiz.app.Metody;
+
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,12 +13,11 @@ import java.util.HashMap;
 public class Historie extends JFrame {
     private ArrayList<String> questions;
     private HashMap<Integer, ArrayList<String>> answers;
-    private HashMap<Integer, Integer> correctAnswers;
-    private int currentQuestionIndex;
+    public static HashMap<Integer, Integer> correctAnswers;
     private JLabel questionLabel;
     private JButton nextButton;
-    public JButton[] answerButtons;
-    public int selectedAnswerIndex = -1;
+    public static JButton[] answerButtons;
+
 
     public Historie() {
         setTitle("Historie");
@@ -38,62 +38,35 @@ public class Historie extends JFrame {
         answerButtons = new JButton[4];
         answerButtons[0] = new JButton();
         answerButtons[0].setBounds(50, 150, 250, 50);
-        answerButtons[0].addActionListener(new ReakceOdpovedi(this,0));
+        answerButtons[0].addActionListener(new Metody.ReakceOdpovedi(0, questions, answers, questionLabel, answerButtons, correctAnswers));
         add(answerButtons[0]);
 
         answerButtons[1] = new JButton();
         answerButtons[1].setBounds(350, 150, 250, 50);
-        answerButtons[1].addActionListener(new ReakceOdpovedi(this,1));
+        answerButtons[1].addActionListener(new Metody.ReakceOdpovedi(1, questions, answers, questionLabel, answerButtons, correctAnswers));
         add(answerButtons[1]);
 
         answerButtons[2] = new JButton();
         answerButtons[2].setBounds(50, 250, 250, 50);
-        answerButtons[2].addActionListener(new ReakceOdpovedi(this,2));
+        answerButtons[2].addActionListener(new Metody.ReakceOdpovedi(2, questions, answers, questionLabel, answerButtons, correctAnswers));
         add(answerButtons[2]);
 
         answerButtons[3] = new JButton();
         answerButtons[3].setBounds(350, 250, 250, 50);
-        answerButtons[3].addActionListener(new ReakceOdpovedi(this,3));
+        answerButtons[3].addActionListener(new Metody.ReakceOdpovedi(3, questions, answers, questionLabel, answerButtons, correctAnswers));
         add(answerButtons[3]);
 
         nextButton = new JButton("Další otázka");
         nextButton.setBounds(50, 400, 150, 50);
-        nextButton.addActionListener(e -> showNextQuestion());
+        nextButton.addActionListener(e -> Metody.showNextQuestion(questions, answers, questionLabel, answerButtons));
         add(nextButton);
 
-        currentQuestionIndex = 0;
-        showNextQuestion();
+        Metody.showNextQuestion(questions, answers, questionLabel, answerButtons);
+
 
         setVisible(true);
     }
 
-    private void showNextQuestion() {
-        if (currentQuestionIndex < questions.size()) {
-            questionLabel.setText(questions.get(currentQuestionIndex));
-            ArrayList<String> currentAnswers = answers.get(currentQuestionIndex + 1);
-            for (int i = 0; i < 4; i++) {
-                answerButtons[i].setText(currentAnswers.get(i));
-                answerButtons[i].setEnabled(true);
-            }
-            selectedAnswerIndex = -1;
-        } else {
-            JOptionPane.showMessageDialog(this, "Konec kvízu!");
-            dispose();
-        }
-        currentQuestionIndex++;
-    }
-
-    public void checkAnswer() {
-        if (selectedAnswerIndex != -1) {
-            int correctIndex = correctAnswers.get(currentQuestionIndex);
-            boolean isCorrect = selectedAnswerIndex == correctIndex;
-            if (isCorrect) {
-                JOptionPane.showMessageDialog(this, "spravne", "ddd", JOptionPane.INFORMATION_MESSAGE);
-            } else {
-                JOptionPane.showMessageDialog(this, "spatne", "sss", JOptionPane.ERROR_MESSAGE);
-            }
-        }
-    }
 
     public static void main(String[] args) {
         new Historie();
---
+++ src/quiz/app/Metody.java
@@ -0,0 +1,71 @@
+package quiz.app;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Metody {
+    private static int currentQuestionIndex = 0;
+    private static int selectedAnswerIndex = -1;
+
+    public static void showNextQuestion(ArrayList<String> questions, HashMap<Integer, ArrayList<String>> answers, JLabel questionLabel, JButton[] answerButtons) {
+        if (currentQuestionIndex < questions.size()) {
+            questionLabel.setText(questions.get(currentQuestionIndex));
+            ArrayList<String> currentAnswers = answers.get(currentQuestionIndex + 1);
+            for (int i = 0; i < answerButtons.length; i++) {
+                answerButtons[i].setText(currentAnswers.get(i));
+                answerButtons[i].setEnabled(true);
+            }
+            selectedAnswerIndex = -1;
+        } else {
+            JOptionPane.showMessageDialog(null, "Konec kvízu!");
+            System.exit(0);
+        }
+        currentQuestionIndex++;
+    }
+
+    public static void checkAnswer(HashMap<Integer, Integer> correctAnswers, int currentQuestionIndex, int selectedAnswerIndex) {
+        if (selectedAnswerIndex != -1) {
+            int correctIndex = correctAnswers.get(currentQuestionIndex - 1); // -1 because currentQuestionIndex was incremented in showNextQuestion
+            boolean isCorrect = selectedAnswerIndex == correctIndex;
+            if (isCorrect) {
+                JOptionPane.showMessageDialog(null, "spravne", "ddd", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "spatne", "sss", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public static class ReakceOdpovedi implements ActionListener {
+        private int index;
+        private ArrayList<String> questions;
+        private HashMap<Integer, ArrayList<String>> answers;
+        private JLabel questionLabel;
+        private JButton[] answerButtons;
+        private HashMap<Integer, Integer> correctAnswers;
+
+        public ReakceOdpovedi(int index, ArrayList<String> questions, HashMap<Integer, ArrayList<String>> answers, JLabel questionLabel, JButton[] answerButtons, HashMap<Integer, Integer> correctAnswers) {
+            this.index = index;
+            this.questions = questions;
+            this.answers = answers;
+            this.questionLabel = questionLabel;
+            this.answerButtons = answerButtons;
+            this.correctAnswers = correctAnswers;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            setSelectedAnswerIndex(index);
+            for (int i = 0; i < answerButtons.length; i++) {
+                answerButtons[i].setEnabled(i == index);
+            }
+            checkAnswer(correctAnswers, currentQuestionIndex, index);
+        }
+    }
+
+    public static void setSelectedAnswerIndex(int index) {
+        selectedAnswerIndex = index;
+    }
+}
