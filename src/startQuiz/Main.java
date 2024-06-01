70f16892966b8aa693ee9641505348422852b012
Jakub <jdnouzov@gmail.com>
2024-06-01 18:42:52 +0200

    Vytvoření tříd pro načítání, zapisování skore a jmena do souboru, ktera se budou objevovat v scoreboardu. Přejmenovávání anglických výrazů do českých. Doplnění JavaDocs do tříd. Metoda na zastavení časovače, protože když se program vypne, po 5 minutách vyskočí oznámení

---
+++ src/startQuiz/Main.java
@@ -0,0 +1,10 @@
+package startQuiz;
+
+
+import quiz.app.Prihlaseni;
+
+public class Main {
+    public static void main(String[] args) {
+        Prihlaseni prihlaseni = new Prihlaseni();
+    }
+}
